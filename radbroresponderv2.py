a791d9756c530b4c341de024a6072d7194b6d699
CryptoSocks <CryptoSocks@users.noreply.github.com>
2023-05-16 02:05:52 -0400

    radbroresponderv2.py Changes: moved the sleep mechanism to its own helper function made sleep based on earliest request time modified requests to use only the tweepy client added looping through paginated requests to loop_responses
    tweeter.py Changes:
    removed remainder of legacy v1 api variables
    added sleeper function
    added get_tweets function
    modified check_user_follows to only request the following list if its been more than 5 minutes since its last request
    modified tweet function to print the username and tweet of the person being responded to

--- radbroresponderv2.py
+++ radbroresponderv2.py
@@ -1,94 +1,55 @@
-from constants import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET, TWITTER_BEARER_TOKEN
-import requests
-import json
-from tweeter import tweet
+from tweeter import tweet, get_tweets, sleeper
 import regex as re
 import time
 import datetime
 
-# To set your environment variables in your terminal run the following line:
-# export 'BEARER_TOKEN'='<your_bearer_token>'
-bearer_token = TWITTER_BEARER_TOKEN
+query = '(radbro OR radbros OR Radbro OR Radbros -is:retweet)'
+expansions = 'author_id'
+last_requests = []
 
 
-search_url = "https://api.twitter.com/2/tweets/search/recent"
-
-# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
-# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
-query_params = {'query': '(radbro OR radbros OR Radbro OR Radbros -is:retweet)', 'expansions': 'author_id', 'max_results': '100'}
-
-
-def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
-
-    r.headers["Authorization"] = f"Bearer {bearer_token}"
-    r.headers["User-Agent"] = "v2RecentSearchPython"
-    return r
-
-def loop_responses(response, previous_id):
-    meta_params = response.json()['meta']
+def loop_responses(response, since_id):
+    global last_requests
+    meta_params = response.meta
+    newest_id = meta_params['newest_id']
     if meta_params['result_count'] == 0:
-        print("no new tweets sleep 3 min")
-        time.sleep(180)
-        return previous_id
+        print("no new tweets sleep 1.5 min")
+        time.sleep(90)
+        return since_id
     else:
-        for response_line in response.json()['data']:
-            if response_line:
-                text = response_line['text'].lower()
-                text = re.sub(r"(?:\@|https?\://)\S+", "", text)
-                text = text.replace(" ", "")
-                user_id = response_line['author_id']
-                id = response_line['id']
-                print("author id: " + user_id)
-                print(text)
-                try:
-                    if "radbro" in text:
-                        tweet(text='radbro', id=id, user_id=user_id)
-                except Exception as e:
-                    print(e)
-        return meta_params['newest_id']
+        while True:
+            for response_line in response.data:
+                if response_line:
+                    text = response_line['text'].lower()
+                    text = re.sub(r"(?:\@|https?\://)\S+", "", text)
+                    user_id = response_line['author_id']
+                    tweet_id = response_line['id']
+                    try:
+                        if "radbro" in text:
+                            tweet(text='radbro', tweet_id=tweet_id, user_id=user_id, tweeted=text)
+                    except Exception as e:
+                        print(e)
+            if response.meta['next_token'] is not None:
+                sleeper(15, last_requests)
+                response = get_tweets(query=query, expansions=expansions, next_token=response.meta['next_token'])
+                last_requests.append(datetime.datetime.now())
+            else:
+                break
+        return newest_id
 
-def connect_to_endpoint(url, params):
-    response = requests.get(url, auth=bearer_oauth, params=params)
-    print(response.status_code)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    for response_line in response.json()['data']:
-        if response_line:
-            text = response_line['text'].lower()
-            text = re.sub(r"(?:\@|https?\://)\S+", "", text)
-            text = text.replace(" ", "")
-            user_id = response_line['author_id']
-            id = response_line['id']
-            print("author id: " + user_id)
-            print(text)
-            try:
-                if "radbro" in text:
-                    tweet(text='radbro', id=id, user_id=user_id)
-            except Exception as e:
-                print(e)
 
-def tl_stream(url):
-    query_params = {'query': '(radbro OR radbros OR Radbro OR Radbros -is:retweet)', 'expansions': 'author_id'}
-    previous_id = False
-    r = 0
+def tl_stream():
+    global last_requests
+    since_id = None
     while True:
-        if r > 15:
-            r = 0
-            print("too much searchy eepy now " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-            print("wake me up at " + (datetime.datetime.now() + datetime.timedelta(minutes=15)).strftime("%d/%m/%Y %H:%M:%S"))
-            time.sleep(900)
-        response = requests.get(url, auth=bearer_oauth, params=query_params)
-        print(response.status_code)
-        if response.status_code != 200:
-            raise Exception(response.status_code, response.text)
-        query_params['since_id'] = previous_id = loop_responses(response, previous_id)
-        r += 1
+        sleeper(15, last_requests)
+        response = get_tweets(query=query, expansions=expansions, since_id=since_id)
+        last_requests.append(datetime.datetime.now())
+        since_id = loop_responses(response, since_id)
+
 
 def main():
-    tl_stream(search_url)
+    tl_stream()
     """
     connect_to_endpoint(search_url, query_params)
     """
--- tweeter.py
+++ tweeter.py
@@ -1,7 +1,9 @@
 from constants import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET, TWITTER_BEARER_TOKEN
+
 import tweepy
 import time
 import datetime
+
 
 client = tweepy.Client(
     consumer_key=CONSUMER_KEY,
@@ -12,70 +14,61 @@ client = tweepy.Client(
     wait_on_rate_limit=True
 )
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-api = tweepy.API(auth, wait_on_rate_limit=True)
-
-
 RESPONDER_ID = 1650231286215196679
 
 followers = []
-rf = 0
-rt = 0
-rl = 0
+last_follow_request = [datetime.datetime.now() - datetime.timedelta(minutes=6)]
+last_tweet_request = []
+last_like_request = []
 
+
+def sleeper(limit, request_list):
+    if len(request_list) == limit and request_list[0] + datetime.timedelta(minutes=15) <= datetime.datetime.now():
+        oldest_request = request_list.pop(0)
+        print("too many requests eepy now " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        sleep_length = (datetime.timedelta(minutes=15) - (datetime.datetime.now() - oldest_request)).seconds + 30
+        print("wake me up at " + (datetime.datetime.now() + datetime.timedelta(seconds=sleep_length)).strftime(
+            "%d/%m/%Y %H:%M:%S"))
+        time.sleep(sleep_length)
+
+
+def get_tweets(query, next_token=None, since_id=None, expansions=None):
+    return client.search_recent_tweets(query=query, next_token=next_token, since_id=since_id, expansions=expansions)
 
 def check_user_follows(user_id):
     global followers
-    global rf
+    global last_follow_request
 
-    print("follow " + str(rf))
-    if rf < 15:
-        rf += 1
-    else:
-        rf = 0
-        print("too much follow eepy now " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-        print("wake me up at " + (datetime.datetime.now() + datetime.timedelta(minutes=15)).strftime("%d/%m/%Y %H:%M:%S"))
-        time.sleep(900)
-    if not any(user_id is str(follow.id) for follow in followers):
-        followers = client.get_users_followers(id=RESPONDER_ID)[0]
-        return any(user_id == str(follow.id) for follow in followers)
+    if not any(user_id == follow.id for follow in followers):
+        if datetime.datetime.now() - last_follow_request[-1] <= datetime.timedelta(minutes=5):
+            return False
+        sleeper(15, last_follow_request)
+        followers = client.get_users_followers(id=RESPONDER_ID, max_results=1000)[0]
+        last_follow_request.append(datetime.datetime.now())
+        return any(user_id == follow.id for follow in followers)
     else:
         return True
 
 
-def tweet(text, id, user_id):
-    global rt
-    global rl
+def tweet(text, tweet_id, user_id, tweeted):
+    global last_tweet_request
+    global last_like_request
+    global followers
     user_follows = check_user_follows(user_id)
     if user_id != RESPONDER_ID and user_follows:
-        print("they follow you, watch out!")
+        username = [user.name for user in followers if user.id == user_id]
         """
         if text == '420':
             media = api.media_upload(f'memes/miladyblunt.jpeg')
             client.create_tweet(text='BLAZE IT', media_ids=[media.media_id], in_reply_to_tweet_id=id)
         else:
         """
-
-        print("tweet " + str(rt))
-        print("like " + str(rl))
-        if rt < 200:
-            rt += 1
-        else:
-            rt = 0
-            print("too much tweety eepy now " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-            print("wake me up at " + (datetime.datetime.now() + datetime.timedelta(minutes=15)).strftime(
-                "%d/%m/%Y %H:%M:%S"))
-            time.sleep(900)
-        if rl < 50:
-            rl += 1
-        else:
-            rl = 0
-            print("too much likey eepy now " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-            print("wake me up at " + (datetime.datetime.now() + datetime.timedelta(minutes=15)).strftime(
-                "%d/%m/%Y %H:%M:%S"))
-            time.sleep(900)
-        client.create_tweet(text=text, in_reply_to_tweet_id=id)
-        client.like(tweet_id=id)
-        print("tweet sent")
+        sleeper(50, last_like_request)
+        sleeper(200, last_tweet_request)
+        print("\n" + username[0] + " said: \n\n" + text)
+        print(tweeted)
+        client.like(tweet_id=tweet_id)
+        last_like_request.append(datetime.datetime.now())
+        client.create_tweet(text=text, in_reply_to_tweet_id=tweet_id)
+        last_tweet_request.append(datetime.datetime.now())
+        print("\nLiked & radbro sent")
