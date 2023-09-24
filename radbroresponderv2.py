c8df0d87fd4153b1b2e5ffd916f0bbcf7ed39b27
CryptoSocks <CryptoSocks@users.noreply.github.com>
2023-09-24 05:58:27 -0400

    - created process_tweet_v2 in order to reduce the redundant calls to send_tweet - reduced the reduced the sleep time from 1.75 min to 30 seconds since we're allowed 60 requests per 15 min - added logs to rate limited interactions to see how many have been made before the limit is hit - created sleeper_v2 to attempt to better control the request flow to not get rate limited on the api side

--- radbroresponderv2.py
+++ radbroresponderv2.py
@@ -1,4 +1,4 @@
-from tweeter import tweet, get_tweets, sleeper
+from tweeter import tweet, get_tweets, sleeper_v2
 import regex as re
 import time
 import datetime
@@ -8,7 +8,7 @@ expansions = 'author_id'
 last_requests = []
 
 
-def send_tweet(text, tweet_id, user_id, tweeted, liked, media_key=None):
+def send_tweet(text, tweet_id, user_id, tweeted, liked=False, media_key=None):
     try:
         tweet(text=text, tweet_id=tweet_id, user_id=user_id, tweeted=tweeted, liked=liked, media_key=media_key)
     except Exception as e:
@@ -21,44 +21,36 @@ def process_tweet(tweet_response):
     user_id = tweet_response['author_id']
     tweet_id = tweet_response['id']
     multi_reply = False
+    shilling = False
     love = ["ily", "ilu", "i love you", "i love u"]
     over = ["its over", "it's over", "it's so over", "its so over"]
     esoterics = "radbro webring is faustian apollonian olympian the extension of infinite space the struggle against elementary chthonic forces the construction of grand murals in his glory the war to subdue lower projects its a quintessentially aryan experience"
-    shilling = False
     if "拉德兄弟" in text:
-        send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                   media_key="./pics/radbroChinese.png")
+        send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/radbroChinese.png")
     elif "拉德猫" in text:
-        send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                   media_key="./pics/radcatChinese.png")
+        send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/radcatChinese.png")
     elif "radcat" in text:
         if "420" in text:
-            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply, media_key="./pics/420.jfif")
+            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/420.jfif")
         elif "1080" in text:
-            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                       media_key="./pics/1080.jfif")
+            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/1080.jfif")
         elif "based" in text:
-            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                       media_key="./pics/based.jpg_medium")
+            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/based.jpg_medium")
         elif "free video" in text:
-            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                       media_key="./pics/radcatvideo.jfif")
+            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/radcatvideo.jfif")
         elif "check the chain" in text:
-            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                       media_key="./pics/checkthechaincat.jfif")
+            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/checkthechaincat.jfif")
         elif "lore" in text:
-            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                       media_key="./pics/radcatlore.jpg_medium")
+            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/radcatlore.jpg_medium")
         elif "git em" in text:
-            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                       media_key="./pics/catcum.jpg_large")
+            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/catcum.jpg_large")
         else:
-            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply, media_key="./pics/radcat.jfif")
+            send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/radcat.jfif")
     elif "radbro" in text:
         if "$rad" in text:
             send_tweet(
                 text='Buy $RAD on sushiswap \n CA: 0xdDc6625FEcA10438857DD8660C021Cd1088806FB \n Link to Sushiswap: https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xdDc6625FEcA10438857DD8660C021Cd1088806FB \n Link to Dexscreener: https://dexscreener.com/ethereum/0x39940ee99171cdbbfdbd540b987e778dba8734dd',
-                tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply)
+                tweet_id=tweet_id, user_id=user_id, tweeted=text)
             shilling = True
             multi_reply = True
         if "$bro" in text:
@@ -66,51 +58,118 @@ def process_tweet(tweet_response):
                 text='Buy $BRO on sushiswap \n CA: 0x6e08B5D1169765f94d5ACe5524F56E8ac75B77c6 \n Link to Sushiswap: https://app.sushi.com/swap?inputCurrency=0xdDc6625FEcA10438857DD8660C021Cd1088806FB&outputCurrency=0x6e08B5D1169765f94d5ACe5524F56E8ac75B77c6 \n Link to Dexscreener: https://dexscreener.com/ethereum/0xa99245ebaf606644b4674994717b3efa098272fe',
                 tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply)
             shilling = True
-            multi_reply = True
         if not shilling:
             if any(love_expression in text for love_expression in love):
-                send_tweet(text='i love you, radbro', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply)
+                send_tweet(text='i love you, radbro', tweet_id=tweet_id, user_id=user_id, tweeted=text)
             elif "put that on my" in text:
-                send_tweet(text='i put that on my radbro 😤', tweet_id=tweet_id, user_id=user_id, tweeted=text,
-                           liked=multi_reply, media_key="./pics/radbro.png")
+                send_tweet(text='i put that on my radbro 😤', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/radbro.png")
             elif "webring" in text:
-                send_tweet(text=esoterics, tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply)
+                send_tweet(text=esoterics, tweet_id=tweet_id, user_id=user_id, tweeted=text)
             elif "lawyer" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/lawyer.jfif")
-            elif "business" or "businessman" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/wagie.png_medium")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/lawyer.jfif")
+            elif "deal" in text:
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/wagie.png_medium")
             elif "always has been" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/RWO.jfif")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/RWO.jfif")
             elif "🤯" and "🦍" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/mindblownape.jfif")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/mindblownape.jfif")
             elif "🤯" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/mindblown.jfif")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/mindblown.jfif")
             elif "pump it" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/pumpit.png")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/pumpit.png")
             elif "ratio" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/Ratio.png")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/Ratio.png")
             elif "git em" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/cum.jpg_medium")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/cum.jpg_medium")
             elif "check the chain" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/checkthechain.png")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/checkthechain.png")
             elif "ssv1" in text:
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply,
-                           media_key="./pics/union.jpg_large")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/union.jpg_large")
             elif any(over_expression in text for over_expression in over):
-                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text,
-                           liked=multi_reply, media_key="./pics/itsover.jpg_medium")
+                send_tweet(text='', tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key="./pics/itsover.jpg_medium")
             else:
-                send_tweet(text='radbro', tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply)
+                send_tweet(text='radbro', tweet_id=tweet_id, user_id=user_id, tweeted=text)
 
+
+def process_tweet_v2(tweet_response):
+    text = tweet_response['text'].lower()
+    text = re.sub(r"(?:\@|https?\://)\S+", "", text)
+    user_id = tweet_response['author_id']
+    tweet_id = tweet_response['id']
+    reply = ''
+    media = None
+    multi_reply = False
+    shilling = False
+    love = ["ily", "ilu", "i love you", "i love u"]
+    over = ["its over", "it's over", "it's so over", "its so over"]
+    esoterics = "radbro webring is faustian apollonian olympian the extension of infinite space the struggle against elementary chthonic forces the construction of grand murals in his glory the war to subdue lower projects its a quintessentially aryan experience"
+    if "拉德兄弟" in text:
+        media = "./pics/radbroChinese.png"
+    elif "拉德猫" in text:
+        media = "./pics/radcatChinese.png"
+    elif "radcat" in text:
+        if "420" in text:
+            media = "./pics/420.jfif"
+        elif "1080" in text:
+            media = "./pics/1080.jfif"
+        elif "based" in text:
+            media = "./pics/based.jpg_medium"
+        elif "free video" in text:
+            media = "./pics/radcatvideo.jfif"
+        elif "check the chain" in text:
+            media = "./pics/checkthechaincat.jfif"
+        elif "lore" in text:
+            media = "./pics/radcatlore.jpg_medium"
+        elif "git em" in text:
+            media = "./pics/catcum.jpg_large"
+        else:
+            media = "./pics/radcat.jfif"
+    elif "radbro" in text:
+        if "$rad" in text:
+            send_tweet(
+                text='Buy $RAD on sushiswap \n CA: 0xdDc6625FEcA10438857DD8660C021Cd1088806FB \n Link to Sushiswap: https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xdDc6625FEcA10438857DD8660C021Cd1088806FB \n Link to Dexscreener: https://dexscreener.com/ethereum/0x39940ee99171cdbbfdbd540b987e778dba8734dd',
+                tweet_id=tweet_id, user_id=user_id, tweeted=text)
+            shilling = True
+            multi_reply = True
+        if "$bro" in text:
+            send_tweet(
+                text='Buy $BRO on sushiswap \n CA: 0x6e08B5D1169765f94d5ACe5524F56E8ac75B77c6 \n Link to Sushiswap: https://app.sushi.com/swap?inputCurrency=0xdDc6625FEcA10438857DD8660C021Cd1088806FB&outputCurrency=0x6e08B5D1169765f94d5ACe5524F56E8ac75B77c6 \n Link to Dexscreener: https://dexscreener.com/ethereum/0xa99245ebaf606644b4674994717b3efa098272fe',
+                tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply)
+            shilling = True
+        if not shilling:
+            if any(love_expression in text for love_expression in love):
+                reply = 'i love you, radbro'
+            elif "put that on my" in text:
+                reply = 'i put that on my radbro 😤'
+                media = "./pics/radbro.png"
+            elif "webring" in text:
+                reply = esoterics
+            elif "lawyer" in text:
+                media = "./pics/lawyer.jfif"
+            elif "deal" in text:
+                media = "./pics/wagie.png_medium"
+            elif "always has been" in text:
+                media = "./pics/RWO.jfif"
+            elif "🤯" and "🦍" in text:
+                media = "./pics/mindblownape.jfif"
+            elif "🤯" in text:
+                media = "./pics/mindblown.jfif"
+            elif "pump it" in text:
+                media = "./pics/pumpit.png"
+            elif "ratio" in text:
+                media = "./pics/Ratio.png"
+            elif "git em" in text:
+                media = "./pics/cum.jpg_medium"
+            elif "check the chain" in text:
+                media = "./pics/checkthechain.png"
+            elif "ssv1" in text:
+                media = "./pics/union.jpg_large"
+            elif any(over_expression in text for over_expression in over):
+                media = "./pics/itsover.jpg_medium"
+            else:
+                reply = 'radbro'
+    if not shilling:
+        send_tweet(text=reply, tweet_id=tweet_id, user_id=user_id, tweeted=text, media_key=media)
 
 def loop_responses(response, since_id):
     global last_requests
@@ -118,14 +177,14 @@ def loop_responses(response, since_id):
     result_count = meta_params['result_count']
 
     if result_count == 0:
-        print("no new tweets sleep 1.75 min")
-        time.sleep(105)
+        print("no new tweets sleep 30 sec")
+        time.sleep(30)
         return since_id
 
     print("\nThe response has " + str(result_count) + " tweets\n")
     for response_line in response.data:
         if response_line:
-            process_tweet(response_line)
+            process_tweet_v2(response_line)
     return meta_params['newest_id']
 
 
@@ -133,7 +192,8 @@ def tl_stream():
     global last_requests
     since_id = None
     while True:
-        sleeper(60, last_requests)
+        print("Requests in the last 15 min: " + str(len(last_requests)))
+        sleeper_v2(60, last_requests)
         response = get_tweets(query=query, expansions=expansions, since_id=since_id)
         last_requests.append(datetime.datetime.now())
         since_id = loop_responses(response, since_id)
--- tweeter.py
+++ tweeter.py
@@ -1,9 +1,7 @@
 from constants import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET, TWITTER_BEARER_TOKEN
-
 import tweepy
 import time
 import datetime
-
 
 client = tweepy.Client(
     consumer_key=CONSUMER_KEY,
@@ -13,17 +11,13 @@ client = tweepy.Client(
     bearer_token=TWITTER_BEARER_TOKEN,
     wait_on_rate_limit=True
 )
-
 auth = tweepy.OAuth1UserHandler(
     consumer_key=CONSUMER_KEY,
     consumer_secret=CONSUMER_SECRET,
     access_token=ACCESS_TOKEN,
     access_token_secret=ACCESS_TOKEN_SECRET
     )
-
 api = tweepy.API(auth)
-
-
 RESPONDER_ID = 1650231286215196679
 
 last_tweet_request = []
@@ -49,6 +43,20 @@ def sleeper(limit, request_list):
         time.sleep(sleep_length)
 
 
+def sleeper_v2(limit, request_list):
+    current_time = datetime.datetime.now()
+    while request_list and (request_list[0] + datetime.timedelta(minutes=15) < current_time):
+        request_list.pop(0)
+    if len(request_list) >= limit:
+        oldest_request = request_list.pop(0)
+        print("Rate limit hit eepy now " + current_time.strftime("%d/%m/%Y %H:%M:%S"))
+        sleep_length = (datetime.timedelta(minutes=15) - (current_time - oldest_request)).total_seconds() + 30
+        print("sleeping for " + str(sleep_length / 60) + " minutes")
+        print("I'll wake up at " + (current_time + datetime.timedelta(seconds=sleep_length)).strftime(
+            "%d/%m/%Y %H:%M:%S"))
+        time.sleep(sleep_length)
+
+
 def get_tweets(query, next_token=None, since_id=None, expansions=None):
     return client.search_recent_tweets(query=query, next_token=next_token, since_id=since_id, expansions=expansions)
 
@@ -58,15 +66,20 @@ def tweet(text, tweet_id, user_id, media_key, tweeted, liked):
     global last_like_request
     if user_id != RESPONDER_ID:
         print(tweeted)
-        if not liked:
-            sleeper(50, last_like_request)
-            client.like(tweet_id=tweet_id)
-            last_like_request.append(datetime.datetime.now())
-        sleeper(200, last_tweet_request)
+
+        print("Tweets in the last 15 min: " + str(len(last_tweet_request)))
+        sleeper_v2(200, last_tweet_request)
         if media_key:
             media_id = api.media_upload(filename=media_key)
             client.create_tweet(text=text, media_ids=[media_id.media_id_string], in_reply_to_tweet_id=tweet_id)
         else:
             client.create_tweet(text=text, in_reply_to_tweet_id=tweet_id)
-            last_tweet_request.append(datetime.datetime.now())
-            print("\nLiked & radbro sent")
+        last_tweet_request.append(datetime.datetime.now())
+        print('\n radbro sent')
+
+        if not liked:
+            print("Likes in the last 15 min: " + str(len(last_like_request)))
+            sleeper_v2(5, last_like_request)
+            client.like(tweet_id=tweet_id)
+            last_like_request.append(datetime.datetime.now())
+            print("Post Liked\n")
