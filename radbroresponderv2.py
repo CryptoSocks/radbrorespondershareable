c29a124faf7637fd1aebf3e603ce7b1d030e61bb
CryptoSocks <CryptoSocks@users.noreply.github.com>
2023-10-13 07:00:24 -0400

    updated reply to respond with uniswap pairs now

--- radbroresponderv2.py
+++ radbroresponderv2.py
@@ -1,3 +1,4 @@
+import os
 from tweeter import tweet, get_tweets, sleeper_v2
 import regex as re
 import time
@@ -51,13 +52,13 @@ def process_tweet_v2(tweet_response):
     elif "radbro" in text:
         if "$rad" in text:
             send_tweet(
-                text='Buy $RAD on sushiswap \n CA: 0xdDc6625FEcA10438857DD8660C021Cd1088806FB \n Link to Sushiswap: https://app.sushi.com/swap?inputCurrency=ETH&outputCurrency=0xdDc6625FEcA10438857DD8660C021Cd1088806FB \n Link to Dexscreener: https://dexscreener.com/ethereum/0x39940ee99171cdbbfdbd540b987e778dba8734dd',
+                text='Buy $RAD on Uniswap \n CA: 0xdDc6625FEcA10438857DD8660C021Cd1088806FB \n Link to Uniswap: https://app.uniswap.org/swap?outputCurrency=0xddc6625feca10438857dd8660c021cd1088806fb&chain=ethereum \n Link to Dexscreener: https://dexscreener.com/ethereum/0x704160cbd110bcc2b4bf5d71830e590bc5685d3a',
                 tweet_id=tweet_id, user_id=user_id, tweeted=text)
             shilling = True
             multi_reply = True
         if "$bro" in text:
             send_tweet(
-                text='Buy $BRO on sushiswap \n CA: 0x6e08B5D1169765f94d5ACe5524F56E8ac75B77c6 \n Link to Sushiswap: https://app.sushi.com/swap?inputCurrency=0xdDc6625FEcA10438857DD8660C021Cd1088806FB&outputCurrency=0x6e08B5D1169765f94d5ACe5524F56E8ac75B77c6 \n Link to Dexscreener: https://dexscreener.com/ethereum/0xa99245ebaf606644b4674994717b3efa098272fe',
+                text='Buy $BRO on uniswap \n CA: 0x6e08B5D1169765f94d5ACe5524F56E8ac75B77c6 \n Link to Uniswap: https://app.uniswap.com/#/swap?inputCurrency=0xddc6625feca10438857dd8660c021cd1088806fb&outputCurrency=0x6e08B5D1169765f94d5ACe5524F56E8ac75B77c6&chain=ethereum \n Link to Dexscreener: https://dexscreener.com/ethereum/0x0110660ec3fede0425183efd88cf5c4341ffc304',
                 tweet_id=tweet_id, user_id=user_id, tweeted=text, liked=multi_reply)
             shilling = True
         if not shilling:
@@ -114,6 +115,39 @@ def loop_responses(response, since_id):
     return meta_params['newest_id']
 
 
+def loop_responses_v2(response, since_id):
+    global last_requests
+    meta_params = response.meta
+    result_count = meta_params['result_count']
+
+    if result_count == 0:
+        print("no new tweets sleep 30 sec")
+        time.sleep(30)
+        return since_id
+
+    print("\nThe response has " + str(result_count) + " tweets\n")
+    for response_line in response.data:
+        if response_line:
+            process_tweet_v2(response_line)
+
+    # Check if there are more pages of tweets
+    if since_id is not None:
+        while 'next_token' in meta_params:
+            next_token = meta_params['next_token']
+            print("Requests in the last 15 min: " + str(len(last_requests)))
+            sleeper_v2(60, last_requests)
+            response = get_tweets(query=query, expansions=expansions, since_id=since_id, next_token=next_token)
+            last_requests.append(datetime.datetime.now())
+            meta_params = response.meta
+            result_count = meta_params['result_count']
+            print("\nThe response has " + str(result_count) + " tweets\n")
+            for response_line in response.data:
+                if response_line:
+                    process_tweet_v2(response_line)
+
+    return meta_params['newest_id']
+
+
 def tl_stream():
     global last_requests
     since_id = None
@@ -123,6 +157,31 @@ def tl_stream():
         response = get_tweets(query=query, expansions=expansions, since_id=since_id)
         last_requests.append(datetime.datetime.now())
         since_id = loop_responses(response, since_id)
+
+
+def tl_stream_v2():
+    global last_requests
+    since_id_file = "since_id.txt"
+
+    # Load the since_id from file if it exists
+    if os.path.exists(since_id_file):
+        with open(since_id_file, "r") as f:
+            since_id = f.read().strip()
+    else:
+        since_id = None
+
+    while True:
+        try:
+            response = get_tweets(query=query, expansions=expansions, since_id=since_id)
+            since_id = loop_responses_v2(response, since_id)
+        except Exception as e:
+            print(e)
+
+        # Save the since_id to file
+        with open(since_id_file, "w") as f:
+            f.write(str(since_id))
+
+        time.sleep(30)
 
 
 def main():
